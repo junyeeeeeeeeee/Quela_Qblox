d433628f4a2aa0d995e09926879eda86efab127b
junyeeeeeeeeee <55795699+junyeeeeeeeeee@users.noreply.github.com>
2024-03-04 11:18:45 +0800

    1

--- Modularize/00_update_chip_info.py
+++ Modularize/00_update_chip_info.py
@@ -6,8 +6,8 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 import Modularize.chip_data_store as cds
 
 # ============= manually set Parameters =================
-QD_path = ''
+QD_path = "D:\HW\量子元件實驗室\Qblox\Quela codes\Quela_Qblox\Modularize\QD_backup\2024_2_27\SumInfo.pkl"
 # ======================put in MeasFlow====================
 
 chip_file = cds.Chip_file()
-# chip_file.update_QD(QD_path)
+chip_file.update_QD(QD_path)
