1fe87d782283afeee75413f6afa1cbb2935bf4df
RatisWu <61738702+RatisWu@users.noreply.github.com>
2024-06-05 22:59:11 +0800

    pp

--- Modularize/analysis/Radiator/RadiatorSyncCheck.py
+++ Modularize/analysis/Radiator/RadiatorSyncCheck.py
@@ -10,7 +10,7 @@ target_q:str = 'q0'
 exp_catas:list = ["effT"]
 sample_folder_name:str = "Radiator_wisconsinQ1"
 conditional_folder_name:str = "Radiator_WS" 
-log_info_dict:dict = {"re0K":{"start_date":"", "start_time":"", "avg_min_from_the_end":60}}
+log_info_dict:dict = {"10K":{}}
 
 for temperature in log_info_dict:
     tempera_folder = os.path.join(meas_raw_dir,sample_folder_name,conditional_folder_name,temperature)
