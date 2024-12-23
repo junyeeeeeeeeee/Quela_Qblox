774e4ece7161c99048d663d65325f9dbb1051dcd
RatisWu <61738702+RatisWu@users.noreply.github.com>
2024-12-23 20:19:25 +0800

    Manually modified pi-len and pi-amp in Manuall_QD_manage.py

--- qblox_drive_AS/Configs/Manuall_QD_manage.py
+++ qblox_drive_AS/Configs/Manuall_QD_manage.py
@@ -9,6 +9,7 @@ from qblox_drive_AS.support.QDmanager import find_path_by_port
 class QD_modifier():
     def __init__(self,QD_path:str):
         self.to_modifiy_item = []
+        self.QD_path = QD_path
         self.QD_agent = QDmanager(QD_path)
         self.QD_agent.QD_loader()
 
@@ -108,6 +109,20 @@ class QD_modifier():
                     self.QD_agent.quantum_device.get_element(q).measure.pulse_amp( self.QD_agent.quantum_device.get_element(q).measure.pulse_amp()*float(roAmp_coef_dict[q]))
                 self.to_modifiy_item.append("RO_amp")
 
+    def set_XY_amp(self, pi_amps:dict={}):
+        if pi_amps is not None:
+            if len(list(pi_amps.keys())) != 0:
+                for q in pi_amps:
+                    self.QD_agent.quantum_device.get_element(q).rxy.amp180(pi_amps[q]) #rxy.duration
+                self.to_modifiy_item.append("PI-amp")
+    
+    def set_XY_duration(self, pi_duras:dict={}):
+        if pi_duras is not None:
+            if len(list(pi_duras.keys())) != 0:
+                for q in pi_duras:
+                    self.QD_agent.quantum_device.get_element(q).rxy.duration(pi_duras[q]) #rxy.duration
+                self.to_modifiy_item.append("PI-duration")
+
     def update_coupler_bias(self,cp_elements:dict):
         """
         Update the idle bias in Fluxmanager for couplers.\n
@@ -130,16 +145,45 @@ class QD_modifier():
         else:
             slightly_print("Your modifications got denied !")
 
+    def export(self, target_q:list=[]):
+        """ Give a list contains qubit names you want to read info like: ['q0','q1'] or you can set target_q = 'all' to print all qubits."""
+        if isinstance(target_q,str):
+            if target_q.lower() == 'all':
+                qs = self.QD_agent.quantum_device.elements()
+        else:
+            qs = target_q
+        
+        if len(qs) != 0:
+            with open(os.path.join("qblox_drive_AS","QD_info.toml"), "w") as file:
+                file.write(f"QD_file: {self.QD_path} infomation.\n\n")
+
+                for q in qs:
+                    file.write(f'[{q}]\n')  
+                    qubit = self.QD_agent.quantum_device.get_element(q)
+                    file.write(f"    bare   = {self.QD_agent.Notewriter.get_bareFreqFor(q)*1e-9} GHz\n")
+                    file.write(f"    ROF    = {qubit.clock_freqs.readout()*1e-9} GHz\n")
+                    file.write(f"    XYF    = {qubit.clock_freqs.f01()*1e-9} GHz\n")
+                    file.write(f"    Pi-amp = {qubit.rxy.amp180()} V\n")
+                    file.write(f"    Pi-dura= {round(qubit.rxy.duration()*1e9,0)} ns\n")
+                    file.write(f"    x      = {(qubit.clock_freqs.readout()-self.QD_agent.Notewriter.get_bareFreqFor(q))*1e-6} MHz\n")
+                    file.write(f"    g      = {self.QD_agent.Notewriter.get_sweetGFor(q)*1e-6} MHz\n")
+
+
 if __name__ == "__main__":
 
-    QD_path = "qblox_drive_AS/QD_backup/20241209/DR1#11_SumInfo.pkl"
+    QD_path = "qblox_drive_AS/QD_backup/20241211/DR1#11_SumInfo.pkl"
     QMaster = QD_modifier(QD_path)
 
+    ### Readout
     """ Set RO amp by a coef. """
     QMaster.set_ROamp_by_coef(roAmp_coef_dict={}) # roAmp_coef_dict = {"q0":0.93, "q1":0.96, ...}, set None or {} to bypass 
 
     """ Set RO freq """
     QMaster.set_ROF(ROFs={})                      # ROFs = {"q0":6.0554e9, .....}
+
+    """ Set RO-LO, RO-atte ( target_q QRM-RF modlue global) """
+    QMaster.set_roLOfreq(LO_Hz=None, target_q='q0') # LO is global in the same QRM-RF module, set None to bypass 
+    QMaster.set_roAtte(ro_atte=None, target_q='q0') # RO-attenuation is global in the same QRM-RF module, set None to bypass 
     
     """ Set Integration time """ 
     QMaster.set_integration_time(inte_time_s={}) # inte_time_s = {"q0":1e-6, "q1":0.75e-6, ...}, set None or {} to bypass 
@@ -147,16 +191,26 @@ if __name__ == "__main__":
     """ Set reset time (All qubits global) """
     QMaster.setGlobally_reset_time(reset_time_s=None)      # reset_time_s = 250e-6, all the qubit in the quantum_device will share the same value
 
+    ### Driving 
+    """ Set pi pulse amplitude """
+    QMaster.set_XY_amp(pi_amps = {})                      # pi_amps = {"q0":0.2, "q1":0.15, ...}
+
+    """ Set pi pulse duration """
+    QMaster.set_XY_duration(pi_duras = {})                # pi_duras = {"q0":40e-9, "q1":48e-9, ...}
+
     """ Set driving attenuation (All qubits blobal) """     # xy_atte = 10 recommended, all qubits are shared with a same value. Must be multiples of 2.
     QMaster.setGlobally_driving_atte(xy_atte=None)
 
     """ Set driving IF """
     QMaster.set_drivin_IF(driving_IF_Hz={})   # driving_IF_Hz = {"q0":-150e6, "q1":-100e6, ...}, set None or {} to bypass  !!! Always be negative !!!
 
-    """ Set RO-LO, RO-atte ( target_q QRM-RF modlue global) """
-    QMaster.set_roLOfreq(LO_Hz=None, target_q='q0') # LO is global in the same QRM-RF module, set None to bypass 
-    QMaster.set_roAtte(ro_atte=None, target_q='q0') # RO-attenuation is global in the same QRM-RF module, set None to bypass 
+    """ Set Darg coef """
+    QMaster.set_drag_coef(Coefs={})    # Coefs = {"q0": -0.5, ....}
 
+    """ Set T2 use detuing, unit: Hz """
+    QMaster.set_RamseyT2detuing(detunes={})   # detunes = {"q0": -0.5e6, ....}
+
+    ### z and others
     """ Set coupler bias """
     QMaster.update_coupler_bias(cp_elements={})  # cp_elements = {"c0":0.1, "c2":0.05, ...}, set None or {} to bypass 
     
@@ -166,11 +220,10 @@ if __name__ == "__main__":
     """ Set sweet spot bias """
     QMaster.set_sweet_bias(offsets={})          # offsets = {"q0": 0.08, ....}
 
-    """ Set Darg coef """
-    QMaster.set_drag_coef(Coefs={})    # Coefs = {"q0": -0.5, ....}
-
-    """ Set T2 use detuing, unit: Hz """
-    QMaster.set_RamseyT2detuing(detunes={})   # detunes = {"q0": -0.5e6, ....}
 
 
+    ### Export a toml to see the QD info
+    QMaster.export(target_q="all")         # target_q = 'all' or ['q0', 'q1', ...]     
+
+    
     QMaster.save_modifications()
