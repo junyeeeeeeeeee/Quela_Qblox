b07bf6f2ab64fd6c61dd04385af3989539873c7f
RatisWu <61738702+RatisWu@users.noreply.github.com>
2025-01-23 22:55:49 +0800

    pp

--- qblox_drive_AS/Executions/m14_EnergyRelaxation.py
+++ qblox_drive_AS/Executions/m14_EnergyRelaxation.py
@@ -6,17 +6,17 @@ from qblox_drive_AS.support.ExpFrames import EnergyRelaxation
 
 ''' fill in '''
 Execution:bool = True
-DRandIP = {"dr":"dr2","last_ip":"10"}
-time_range:dict = {"q0":[0,80e-6],"q1":[0,70e-6]}
+DRandIP = {"dr":"dr1","last_ip":"11"}
+time_range:dict = {"q0":[0,20e-6]}
 time_sampling_func:str = "linspace"
 time_ptsORstep:int|float = 100
 AVG:int = 500
-histo_counts:int = 1
-use_OnsShot:bool = False
+histo_counts:int = 100
+use_OneShot:bool = False
 
 ''' Don't Touch '''
 save_dir = Data_manager().build_packs_folder()
 EXP = EnergyRelaxation(QD_path=find_latest_QD_pkl_for_dr(DRandIP["dr"],DRandIP["last_ip"]),data_folder=save_dir)
-EXP.SetParameters(time_range,time_sampling_func,time_ptsORstep,histo_counts,AVG,Execution,use_OnsShot)
+EXP.SetParameters(time_range,time_sampling_func,time_ptsORstep,histo_counts,AVG,Execution,use_OneShot)
 EXP.WorkFlow()
 EXP.RunAnalysis()
