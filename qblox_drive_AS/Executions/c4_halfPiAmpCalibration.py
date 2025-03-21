53643b966928c09ca98dc368c2b61563137d1e5f
RatisWu <61738702+RatisWu@users.noreply.github.com>
2025-03-21 14:00:02 +0800

    decorations in execution.c4

--- qblox_drive_AS/Executions/c4_halfPiAmpCalibration.py
+++ qblox_drive_AS/Executions/c4_halfPiAmpCalibration.py
@@ -7,7 +7,7 @@ from qblox_drive_AS.support.ExpFrames import hPiAcali
 ''' fill in '''
 Execution:bool = 1
 DRandIP = {"dr":"dr1","last_ip":"11"}
-ro_power_coef_range:dict = {"q4":[0.9,1.1]}    # half pi pulse coef, it should be around 0.5
+half_pi_coef_range:dict = {"q4":[0.9,1.1]}    # half pi pulse coef, it should be around 1
 coef_sampling_func:str = 'linspace'
 half_pi_quadruple_num:list = [5,9]
 coef_ptsORstep:int|float = 50
@@ -18,6 +18,6 @@ AVG:int = 300
 ''' Don't Touch '''
 save_dir = Data_manager().build_packs_folder()
 EXP = hPiAcali(QD_path=find_latest_QD_pkl_for_dr(DRandIP["dr"],DRandIP["last_ip"]),data_folder=save_dir)
-EXP.SetParameters(ro_power_coef_range,coef_sampling_func,coef_ptsORstep,half_pi_quadruple_num,avg_n=AVG,execution=Execution)
+EXP.SetParameters(half_pi_coef_range,coef_sampling_func,coef_ptsORstep,half_pi_quadruple_num,avg_n=AVG,execution=Execution)
 EXP.WorkFlow()
 EXP.RunAnalysis()
