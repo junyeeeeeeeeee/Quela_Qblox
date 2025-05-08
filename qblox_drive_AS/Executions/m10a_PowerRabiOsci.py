a9eab6a1c4aef9bbf39e662a04dfffb5f562dfc3
RatisWu <61738702+RatisWu@users.noreply.github.com>
2025-05-08 18:25:31 +0800

    up

--- qblox_drive_AS/Executions/m10a_PowerRabiOsci.py
+++ qblox_drive_AS/Executions/m10a_PowerRabiOsci.py
@@ -3,7 +3,7 @@ from qblox_drive_AS.support import Data_manager
 from qblox_drive_AS.support.ExpFrames import PowerRabiOsci
 
 ''' fill in '''
-Execution:bool = 1
+Execution:bool = 0
 DRandIP = {"dr":"dr1","last_ip":"11"}
 pi_amp_range:dict = {"q1":[-0.9, 0.9], "q3":[-0.9, 0.9]}    # [pi_amp_start, pi_amp_end]
 pi_amp_sampling_func:str = 'linspace'                          # 'linspace'/ 'logspace'/ 'arange
@@ -11,7 +11,7 @@ pi_amp_ptsORstep:int|float = 100  # Depends on the sampling func you use, 'linsp
 AVG:int = 1000
 
 #?? Notes: While you're using one-shot method, amp samples (pi_amp_pts) * shots (AVG) must less than or equal to 131000. 
-use_OneShot:bool = True
+use_OneShot:bool = False
 
 ''' Don't Touch '''
 save_dir = Data_manager().build_packs_folder()
